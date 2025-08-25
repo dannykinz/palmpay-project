fd29a673aa3689e471168b2a4c52a6f339837811
Danny Kinz <talktodknow@gmail.com>
2025-08-25 11:10:21 +0100

    mobile menu implemented

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-console.log("Hello, Mom");
+const openMenu =  document.querySelector(".hamburger-icon");
+const closeMenu = document.querySelector(".hamburger-icon-close");
+const mobileMenu = document.querySelector(".mobile-menu-links");
 
-alert("Hello, Mom");
+openMenu.addEventListener("click", () => {
+    // Open Menu Default Actions
+        openMenu.style.display = "none";
+        closeMenu.style.display = "block";
+        mobileMenu.style.display = "flex";
 
-let name = "Danny Kinz"
+    // Close Menu Event
+    closeMenu.addEventListener("click", () => {
+            openMenu.style.display = "block";
+            closeMenu.style.display = "none";
+            mobileMenu.style.display = "none";
+    });
+})
